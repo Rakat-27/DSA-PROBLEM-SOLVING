909b65bf3f600a443adb2f237ff7ff735ee85304
Rakat-27 <syedrafiulislam627@gmail.com>
2025-06-03 03:17:29 +0600

    void function in java

---
+++ function.java
@@ -0,0 +1,14 @@
+public class function {
+
+    public static void printHello() {
+        System.out.println("Hello, Rakat!");
+    }
+
+    public static void greeting(){
+        System.out.println("Eid Mubarak Brothers and Sisters!");
+    }
+    public static void main(String[] args) {
+        printHello();
+        // greeting(); 
+    }
+}
